60b3fcdda4b203fa5a99b7b56e08ad97b9c6ac12
modestalmirall <164864991+modestalmirall@users.noreply.github.com>
2024-04-18 21:40:48 +0200

    El nou

---
+++ nou/js/app.js
@@ -0,0 +1,30 @@
+// menú
+
+document.querySelector('.boto').addEventListener('click', function(e) {
+	e.preventDefault();
+	document.documentElement.classList.toggle('menu');
+	document.documentElement.classList.toggle('nomenu');
+});
+
+//any
+document.getElementById("_any_").innerHTML = new Date().getFullYear();
+
+// slider
+document.addEventListener('DOMContentLoaded', function() {
+  let currentImageIndex = 0;
+  const images = document.querySelectorAll('.background-slider img');
+  const totalImages = images.length;
+
+  function showNextImage() {
+	const currentImage = images[currentImageIndex];
+	currentImage.style.opacity = '0';
+
+	currentImageIndex = (currentImageIndex + 1) % totalImages;
+
+	const nextImage = images[currentImageIndex];
+	nextImage.style.opacity = '1';
+  }
+
+  // Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
+  setInterval(showNextImage, 3000);
+});
