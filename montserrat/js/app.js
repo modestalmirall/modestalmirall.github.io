baf41e3d128560573c73f0eb1cdf722ff49431c1
modestalmirall <164864991+modestalmirall@users.noreply.github.com>
2024-04-25 00:45:00 +0200

    Actualització Montserrat

--- montserrat/js/app.js
+++ montserrat/js/app.js
@@ -1,5 +1,4 @@
 // menú
-
 document.querySelector('.boto').addEventListener('click', function(e) {
 	e.preventDefault();
 	document.documentElement.classList.toggle('menu');
@@ -10,6 +9,7 @@ document.querySelector('.boto').addEventListener('click', function(e) {
 document.getElementById("_any_").innerHTML = new Date().getFullYear();
 
 // slider
+/*
 document.addEventListener('DOMContentLoaded', function() {
   let currentImageIndex = 0;
   const images = document.querySelectorAll('.fonsPortada  img');
@@ -28,3 +28,28 @@ document.addEventListener('DOMContentLoaded', function() {
   // Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
   setInterval(showNextImage, 3000);
 });
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+	let currentImageIndex = 0;
+	const images = document.querySelectorAll('.fonsPortada img');
+  
+	// Comprova si l'element .fonsPortada existeix abans de continuar
+	if (images.length > 0) {
+	  const totalImages = images.length;
+  
+	  function showNextImage() {
+		const currentImage = images[currentImageIndex];
+		currentImage.style.opacity = '0';
+  
+		currentImageIndex = (currentImageIndex + 1) % totalImages;
+  
+		const nextImage = images[currentImageIndex];
+		nextImage.style.opacity = '1';
+	  }
+  
+	  // Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
+	  setInterval(showNextImage, 3000);
+	}
+  });
+  
