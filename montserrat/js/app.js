ed496dd7c5c2ae991ecc931b56f418402860430d
modestalmirall <164864991+modestalmirall@users.noreply.github.com>
2024-04-25 18:08:03 +0200

    trastejant amb Montserrat

--- montserrat/js/app.js
+++ montserrat/js/app.js
@@ -9,47 +9,72 @@ document.querySelector('.boto').addEventListener('click', function(e) {
 document.getElementById("_any_").innerHTML = new Date().getFullYear();
 
 // slider
+document.addEventListener('DOMContentLoaded', function() {
+	let currentImageIndex = 0;
+	const images = document.querySelectorAll('.fonsPortada img');
+
+	// Comprova si l'element .fonsPortada existeix abans de continuar
+	if (images.length > 0) {
+		const totalImages = images.length;
+
+		function showNextImage() {
+			const currentImage = images[currentImageIndex];
+			currentImage.style.opacity = '0';
+
+			currentImageIndex = (currentImageIndex + 1) % totalImages;
+
+			const nextImage = images[currentImageIndex];
+			nextImage.style.opacity = '1';
+		}
+
+		// Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
+		setInterval(showNextImage, 3000);
+	}
+});
+
+// Masonry
 /*
 document.addEventListener('DOMContentLoaded', function() {
-  let currentImageIndex = 0;
-  const images = document.querySelectorAll('.fonsPortada  img');
-  const totalImages = images.length;
-
-  function showNextImage() {
-	const currentImage = images[currentImageIndex];
-	currentImage.style.opacity = '0';
-
-	currentImageIndex = (currentImageIndex + 1) % totalImages;
-
-	const nextImage = images[currentImageIndex];
-	nextImage.style.opacity = '1';
-  }
-
-  // Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
-  setInterval(showNextImage, 3000);
+	// Comprova si l'element '.galeria' existeix a la pàgina
+	var galeria = document.querySelector('.galeria');
+	if (galeria) {
+		// Inicialitza Masonry només si l'element '.galeria' existeix
+		var masonry = new Masonry(galeria, {
+			// Opcions de configuració de Masonry
+			itemSelector: '.quadroGaleria',
+			columnWidth: '.quadroGaleria',
+			gutter: 20, // Espai entre les columnes
+		});
+	}
 });
 */
 
 document.addEventListener('DOMContentLoaded', function() {
-	let currentImageIndex = 0;
-	const images = document.querySelectorAll('.fonsPortada img');
-  
-	// Comprova si l'element .fonsPortada existeix abans de continuar
-	if (images.length > 0) {
-	  const totalImages = images.length;
-  
-	  function showNextImage() {
-		const currentImage = images[currentImageIndex];
-		currentImage.style.opacity = '0';
-  
-		currentImageIndex = (currentImageIndex + 1) % totalImages;
-  
-		const nextImage = images[currentImageIndex];
-		nextImage.style.opacity = '1';
-	  }
-  
-	  // Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
-	  setInterval(showNextImage, 3000);
-	}
-  });
-  
+  // Comprova si l'element '.galeria' existeix a la pàgina
+  var galeria = document.querySelector('.galeria');
+  if (galeria) {
+	// Inicialitza Masonry només si l'element '.galeria' existeix
+	var masonry = new Masonry(galeria, {
+	  // Opcions de configuració de Masonry
+	  itemSelector: '.quadroGaleria',
+	  columnWidth: '.ampleQuadro',
+	  gutter: '.margeQuadro',
+	  percentPosition: true
+	});
+  }
+});
+
+/*
+var elem = document.querySelector('.galeria');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.quadroGaleria',
+  columnWidth: 250
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.quadroGaleria', {
+  // options
+});
+*/
