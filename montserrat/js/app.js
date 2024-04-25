29c33017ba31d5eb658e6375dad9b7139c3dba5b
modestalmirall <164864991+modestalmirall@users.noreply.github.com>
2024-04-25 19:58:44 +0200

    Actualització Montserrat

--- montserrat/js/app.js
+++ montserrat/js/app.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 */
 
-
+/*
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -100,3 +100,53 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Menú
+  document.querySelector('.boto').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.documentElement.classList.toggle('menu');
+    document.documentElement.classList.toggle('nomenu');
+  });
+
+  // Any
+  document.getElementById("_any_").innerHTML = new Date().getFullYear();
+
+  // Slider
+  let currentImageIndex = 0;
+  const images = document.querySelectorAll('.fonsPortada img');
+
+  // Comprova si l'element .fonsPortada existeix abans de continuar
+  if (images.length > 0) {
+    const totalImages = images.length;
+
+    function showNextImage() {
+      const currentImage = images[currentImageIndex];
+      currentImage.style.opacity = '0';
+
+      currentImageIndex = (currentImageIndex + 1) % totalImages;
+
+      const nextImage = images[currentImageIndex];
+      nextImage.style.opacity = '1';
+    }
+
+    // Inicia el bucle per canviar les imatges cada 5 segons (5000 ms)
+    setInterval(showNextImage, 3000);
+  }
+
+  // Masonry
+  var galeria = document.querySelector('.galeria');
+  if (galeria) {
+    // Utilitzo imagesLoaded per assegurar-me que les imatges s'hagin carregat abans de inicialitzar Masonry
+    imagesLoaded(galeria, function() {
+      var masonry = new Masonry(galeria, {
+        // Opcions de configuració de Masonry
+        itemSelector: '.quadroGaleria',
+        columnWidth: '.ampleQuadro',
+        gutter: '.margeQuadro',
+        percentPosition: true
+      });
+    });
+  }
+});
